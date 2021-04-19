85d43e2e0154bdf41185ffdb0537e7a43ee2868e
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-04-19 10:05:02 +0300

    18.04

---
+++ AracTakip/Controllers/HaritaController.cs
@@ -0,0 +1,45 @@
+﻿using AracTakip.Models;
+using Eselfware.Repository.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AracTakip.Controllers
+{
+    public class HaritaController : Controller
+    {
+        UnitOfWork _unitOfWork = new UnitOfWork();
+        string s1 = "";
+        string s2 = "";
+        // GET: Harita
+        [Route("harita-getir")]
+        public ActionResult Harita(string plaka, string TC)
+        {
+            var musteri = _unitOfWork.Musteri.Find(x => x.MusteriTC == TC)._id;
+            var arac = _unitOfWork.Arac.Find(x => x.MusteriID == musteri)._id;
+            var cihaz = _unitOfWork.Cihaz.Find(x => x._id == arac && x.CihazAd == plaka);
+            var XDeger = cihaz.XKonum.ToString();
+            var YDeger = cihaz.YKonum.ToString();
+    
+            return Json(cihaz._id);
+            
+
+        }
+        [Route("get-harita")]
+        public ActionResult GetHarita(string id)
+        {
+            var cihaz = _unitOfWork.Cihaz.Find(x => x._id == id);
+            Locations locations = new Locations
+            {
+                Lat = cihaz.XKonum.ToString(),
+                Long = cihaz.YKonum.ToString()
+            };
+            
+            return View(locations);
+        }
+
+
+    }
+}
---
+++ AracTakip/Models/Locations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AracTakip.Models
+{
+    public class Locations
+    {
+        public string Lat { get; set; }
+        public string Long { get; set; }
+    }
+}
